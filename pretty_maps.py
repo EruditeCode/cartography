eecf3b2915db99fdcd8cc47dac459ac0a128f61f
EruditeCode <erudite.home@gmail.com>
2023-02-16 13:14:22 +0000

    Testing different presets and circle function.

--- pretty_maps.py
+++ pretty_maps.py
@@ -11,8 +11,10 @@ from matplotlib import pyplot as plt
 plot = prettymaps.plot(
     # Carcassonne Coordinates
     '43.206412477118896, 2.3639343079689494',
-    preset = 'default'
+    circle = True,
+    radius = 500,
+    preset = 'minimal'
 )
 
-plt.savefig('test.jpg')
+plt.savefig('test2.jpg')
 plt.show()
